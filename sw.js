31c18e9fbf1f48718a453cc973317e422a886581
Jerry <45614605+jzca@users.noreply.github.com>
2018-12-20 19:30:12 -0600

    Test of deleting unused caches.

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // How to put things into Caches, during installing, before activate.
 // Open or If no Cache, create it. index be detailed
-const staticCache = 'my-cache-1';
+const staticCache = 'my-cache-a';
 
 self.addEventListener('activate', event => {
 	event.waitUntil(
